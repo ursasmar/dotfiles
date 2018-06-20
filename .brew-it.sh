2295990eaca239f859671ed9ed317d04740b4422
Joshua B <joshua@ecomevolution.com>
2018-06-20 18:37:23 -0400

    New additions for Valet Remove legacy commands

--- .brew-it.sh
+++ .brew-it.sh
@@ -22,17 +22,10 @@ brew install bash
 # Install wget with IRI support
 brew install wget --with-iri
 
-# Install RingoJS and Narwhal
-# Note that the order in which these are installed is important; see http://git.io/brew-narwhal-ringo.
-brew install ringojs
-brew install narwhal
-
 # Install more recent versions of some OS X tools
 brew install vim --override-system-vi
 brew install homebrew/dupes/grep
 brew install homebrew/dupes/screen
-brew install homebrew/php/php56 --with-gmp
-brew install homebrew/php/php56-xdebug
 
 # Install other useful stuff
 brew install ack
@@ -49,9 +42,41 @@ brew install zsh-completions
 brew install qcachegrind
 brew linkapps
 
-brew unlink bash-completion # Just in case to prevent a conflict.
-brew tap homebrew/versions
-brew install bash-completion2
+# Install dev tools
+brew install php
+brew install mysql
+brew install postgresql
+brew install redis
+composer global require laravel/valet
+valet install
+sudo php /usr/lib/php/install-pear-nozlib.phar -d /usr/local/lib/php -b /usr/local/bin
+pecl install xdebug
+
+# Config Xdebug
+echo "zend_extension=xdebug.so
+xdebug.var_display_max_data = -1
+xdebug.var_display_max_depth = 100
+xdebug.default_enable = 0
+xdebug.remote_enable = 1
+xdebug.remote_connect_back = 1
+xdebug.remote_port = 9001
+xdebug.remote_host = localhost
+xdebug.remote_autostart = 1
+xdebug.remote_handler = dbgp
+xdebug.idekey = PHPSTORM
+xdebug.collect_return = 1
+xdebug.collect_params = 10
+xdebug.show_mem_delta = 1
+xdebug.profiler_enable_trigger = 1
+xdebug.profiler_output_dir = '~/Projects/xdebug_profiler'
+xdebug.trace_enable_trigger = 1
+xdebug.trace_output_dir = '~/Projects/xdebug_traces'
+xdebug.profiler_enable = 0
+xdebug.trace_enable = 1
+xdebug.cli_color = 1
+xdebug.remote_log = '~/Projects/logs/xdebug_remote.log'" > /usr/local/etc/php/7.2/conf.d/ext-xdebug.ini
+
+sudo brew service restart php
 
 # Remove outdated versions from the cellar
 brew cleanup
@@ -60,45 +85,34 @@ brew install caskroom/cask/brew-cask
 brew tap caskroom/versions
 
 # Install casks.
-brew cask install adobe-air 2> /dev/null
 brew cask install alfred 2> /dev/null
 brew cask install amazon-music 2> /dev/null
-brew cask install crashplan 2> /dev/null
 brew cask install dropbox 2> /dev/null
 brew cask install evernote 2> /dev/null
-brew cask install flux 2> /dev/null
-brew cask install gimp 2> /dev/null
-brew cask install google-chrome 2> /dev/null
 brew cask install google-drive 2> /dev/null
-brew cask install go2shell 2> /dev/null
 brew cask install imagealpha 2> /dev/null
 brew cask install imageoptim 2> /dev/null
 brew cask install iterm2 2> /dev/null
 brew cask install java 2> /dev/null
-brew cask install java6 2> /dev/null # Needed for PHPStorm 8 under Yosemite.
-#brew cask install macvim 2> /dev/null
-brew cask install miro-video-converter 2> /dev/null
-brew cask install mysqlworkbench 2> /dev/null
-brew cask install odesk-team 2> /dev/null
-brew cask install p4merge 2> /dev/null
-brew cask install paragon-ntfs 2> /dev/null
-brew cask install parallels 2> /dev/null
-brew cask install phpstorm 2> /dev/null
 brew cask install skype 2> /dev/null
 brew cask install slack 2> /dev/null
 brew cask install sourcetree 2> /dev/null
 brew cask install sublime-text 2> /dev/null
 brew cask install the-unarchiver 2> /dev/null
-brew cask install teamviewer 2> /dev/null
-brew cask install textexpander 2> /dev/null
 brew cask install torbrowser 2> /dev/null
-brew cask install transmission 2> /dev/null
-brew cask install ukelele 2> /dev/null
 brew cask install utorrent 2> /dev/null
 brew cask install vagrant 2> /dev/null
 brew cask install vagrant-manager 2> /dev/null
 brew cask install virtualbox 2> /dev/null
 brew cask install vlc 2> /dev/null
-brew cask install xtrafinder 2> /dev/null
+
+# Install blackfire
+brew tap blackfireio/homebrew-blackfire
+brew install blackfire-agent
+brew install blackfire-php72
+echo "Config Blackfire by following these instructions: https://blackfire.io/docs/up-and-running/installation"
+
+# Create locate DB
+sudo launchctl load -w /System/Library/LaunchDaemons/com.apple.locate.plist
 
 echo "Run mackup restore when ready!"
--- .dev-tools.sh
+++ .dev-tools.sh
@@ -12,16 +12,6 @@ yo --version && bower --version && grunt --version
 # Install the Chrome extension generator for Yeoman.
 npm install -g generator-chrome-extension
 
-# Set p4merge as default Git diff tool.
-git config --global merge.tool p4mergetool
-git config --global mergetool.p4mergetool.trustExitCode false
-git config --global mergetool.p4mergetool.cmd "$HOME/Applications/p4merge.app/Contents/Resources/launchp4merge \$PWD/\$BASE \$PWD/\$REMOTE \$PWD/\$LOCAL \$PWD/\$MERGED"
-git config --global difftool.p4mergetool.cmd "$HOME/Applications/p4merge.app/Contents/Resources/launchp4merge \$LOCAL \$REMOTE"
-git config --global difftool.sourcetree.cmd "$HOME/Applications/p4merge.app/Contents/Resources/launchp4merge \$LOCAL \$REMOTE"
-git config --global difftool.sourcetree.path ""
-git config --global mergetool.sourcetree.cmd "$HOME/Applications/p4merge.app/Contents/Resources/launchp4merge \$PWD/\$BASE \$PWD/\$REMOTE \$PWD/\$LOCAL \$PWD/\$MERGED"
-git config --global mergetool.sourcetree.trustexitcode true
-
 # Generate new SSH key.
 ssh-keygen -t rsa -b 4096 -C "joshua@ecomevolution.com"
 
